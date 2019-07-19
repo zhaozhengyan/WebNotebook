26631c0acd188aa9f4ffefd600fb5e8220020a1a
zzy <zhaozhengyan@live.com>
2019-07-19 11:52:42 +0800

    #修改 url

--- WebNotebook.Angular/Program.cs
+++ WebNotebook.Angular/Program.cs
@@ -19,7 +19,6 @@ namespace WebNotebook.Angular
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://*:5001")
                 .UseStartup<Startup>();
     }
 }
