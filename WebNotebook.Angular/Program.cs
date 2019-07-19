f70b770d1171790233fb5b3b4b1ecc1f4034d7a1
zzy <zhaozhengyan@live.com>
2019-07-19 10:19:03 +0800

    # add new port

--- WebNotebook.Angular/Program.cs
+++ WebNotebook.Angular/Program.cs
@@ -19,6 +19,7 @@ namespace WebNotebook.Angular
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:5001")
                 .UseStartup<Startup>();
     }
 }
